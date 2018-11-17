7ae48bc253f1fb6e97d261c50dd385583b8454a2
hiroshi-freeplus <hashimoto_hiroshi@freeplus.co.jp>
2018-11-17 23:06:06 +0900

    簡単なVueアプリ作成

--- contracts/GoodleToken.sol
+++ contracts/GoodleToken.sol
@@ -4,11 +4,20 @@ import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 contract GoodleToken is StandardToken {
   string public name = "GoodleToken";
   string public symbol = "GDL";
-  uint public decimals = 18;
-  uint public InitialSupply = 10000 * (10 ** decimals);
+  // uint public decimals = 18;
+  // uint public InitialSupply = 10000 * (10 ** decimals);
+  uint public InitialSupply = 10000;
 
   constructor() public {
     totalSupply_ = InitialSupply;
     balances[msg.sender] = InitialSupply;
+  }
+
+  function getBalance() public view returns (uint) {
+    return balances[msg.sender];
+  }
+
+  function setBalance(uint num) public {
+    balances[msg.sender] = num;
   }
 }
